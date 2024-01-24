3a1f91544bc28b0911a6b206fb45a11ba590f477
roman <roman.robel@exitb.de>
2024-01-24 14:21:04 +0100

    changes in default exclusion usage from create-single-spa-react

--- packages/generator-single-spa/src/react/templates/webpack.config.js
+++ packages/generator-single-spa/src/react/templates/webpack.config.js
@@ -1,13 +1,14 @@
 const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-react<% if (typescript) { %>-ts<% } %>");
+const singleSpaDefaults = require("webpack-config-exitb-single-spa-react<% if (typescript) { %>-ts<% } %>");
 
 module.exports = (webpackConfigEnv, argv) => {
+  const excludeFull = true;
   const defaultConfig = singleSpaDefaults({
     orgName: "<%= orgName %>",
     projectName: "<%= projectName %>",
     webpackConfigEnv,
     argv,
-  });
+  }, excludeFull);
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
