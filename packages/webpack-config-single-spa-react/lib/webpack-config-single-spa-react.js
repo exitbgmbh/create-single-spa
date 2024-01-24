a50bc6cf2d0e9e873534907e5869201998815cc0
roman <roman.robel@exitb.de>
2024-01-24 13:45:00 +0100

    add static packages to exclude list in webpack config

--- packages/webpack-config-single-spa-react/lib/webpack-config-single-spa-react.js
+++ packages/webpack-config-single-spa-react/lib/webpack-config-single-spa-react.js
@@ -2,14 +2,46 @@ const webpackConfigSingleSpa = require("webpack-config-single-spa");
 
 module.exports = webpackConfigSingleSpaReact;
 
-function webpackConfigSingleSpaReact(opts) {
+const alwaysExternalPackages = [
+    'react',
+    'react-dom',
+    'react-router-dom',
+    'react-hook-form',
+    '@mui/material',
+    '@mui/icons-material',
+    '@emotion/styled',
+    '@emotion/react',
+    'react-i18next',
+    'i18next',
+    'i18next-resources-to-backend',
+    'moment',
+    'uuid'
+]
+
+const conditionalExternalPackages = [
+  '@mui/x-license-pro',
+  '@mui/x-data-grid-pro',
+  '@mui/x-data-grid-premium',
+]
+
+function webpackConfigSingleSpaReact(opts, excludeConditionalPackages = true) {
   const webpackConfigEnv = opts.webpackConfigEnv || {};
 
   opts.react = true;
   const config = webpackConfigSingleSpa(opts);
 
   if (!webpackConfigEnv.standalone) {
-    config.externals.push("react", "react-dom");
+    config.externals = [
+        ...config.externals,
+        ...alwaysExternalPackages
+    ]
+
+    if (excludeConditionalPackages) {
+      config.externals = [
+          ...config.externals,
+          ...conditionalExternalPackages
+      ]
+    }
   }
 
   return config;
